ba9a80e01716ecdceae9d25f968d519963cba938
unknown <04x0xx@gmail.com>
2023-06-22 01:20:48 -0700

    removed inbetween list & added time

--- src/main.rs
+++ src/main.rs
@@ -14,96 +14,79 @@ Main file for running processes
 mod math;
 
 extern crate image;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-const SIZE_X: &'static usize = &255;
-const SIZE_Y: &'static usize = &255;
-const MAX_I : &'static u32 = &1024;
 
-fn set_initial_values(state: &mut [[math::Complex; *SIZE_X]; *SIZE_Y]) {
-    /* 
-    Function sets initial values for the array. 
-    Sets each value to be coordinates of mathematical space
-    */
-
-    fn get_math_value(value: usize, max_ref: usize) -> f64 {
-        // Function for getting a mathematical space
-        // Value from "Value"
-        4f64 * (value as f64) / (max_ref as f64 - 1f64) - 2f64
-    }
-
-    // Itterates through all values and sets real / imaginary values to be coordinates in mathematical space
-    for i in 0..*SIZE_Y {
-        for j in 0..*SIZE_X {
-            state[i][j] = math::Complex {
-                real : get_math_value(j, *SIZE_X),
-                imaginary : get_math_value(i, *SIZE_Y),
-            }
-        }
-    }
+fn get_math_value(value: u32, max_ref: u32) -> f64 {
+    // Function for getting the mathematical space
+    // Value from "value"
+    4f64 * (value as f64) / (max_ref as f64 - 1f64) - 2f64
 }
 
-fn eval_function(state: &mut [[math::Complex; *SIZE_X]; *SIZE_Y]) -> [[u32; *SIZE_X]; *SIZE_Y] {
+fn eval_function(size_x: u32, size_y: u32, max_i: u32) {
     /*
         Function for getting the value of each point
     */
 
-    let mut out_values = [[0u32; *SIZE_X]; *SIZE_Y];
+    fn get_point_value( mut z: math::Complex, max_i: u32 ) -> u32 {
+        /*
+            Function for getting the value of a particular point
+            Returns the amount of iterations completed
+        */
 
-    // Goes through each pixel
-    for i in 0..*SIZE_Y {
-        for j in 0..*SIZE_X {
+        // Sets Initial Value
+        let c = z.clone();
 
-            // Sets Variables
-            let mut z = state[i][j];
-            let c = z.clone();
-
-            // Does Math
-            for iteration in 0..*MAX_I {
-                z = z * z + c;
-                if z.is_greater(2f64) {
-                    out_values[i][j] = iteration;
-                    break
-                }
-                if iteration == *MAX_I - 1 {
-                    out_values[i][j] = *MAX_I;
-                }
+        // Gets Mandelbrot Value
+        for iteration in 0..max_i {
+            z = z * z + c;
+            if z.is_greater(2f64) {
+                return iteration;
             }
         }
+        return max_i;
     }
-    out_values
-}
 
-fn save_image(state: &mut [[u32; *SIZE_X]; *SIZE_Y]) {
-    /* 
-        Function that saves the 2D list of numbers as an image. 
-    */
-
-    let mut img = image::ImageBuffer::new(*SIZE_X as u32, *SIZE_Y as u32);
+    // Sets Image Values
+    let mut img = image::ImageBuffer::new(size_x, size_y);
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let r = (0.2 * x as f32) as u8;
         let b = (0.2 * y as f32) as u8;
         *pixel = image::Rgb([r, 0, b]);
     }
-    for i in 0..*SIZE_Y {
-        for j in 0..*SIZE_X {
+
+    // Goes through each pixel
+    for i in 0..size_y {
+        for j in 0..size_x {
+
+            // Sets Initial Value
+            let z = math::Complex{ real : get_math_value(j, size_x), imaginary : get_math_value(i, size_y), };
+            // Gets output of z value
+            let z_output = get_point_value( z, max_i );
             let pixel = img.get_pixel_mut(j as u32, i as u32);
             let image::Rgb(data) = *pixel;
-            let n = (255 * state[i][j] / *MAX_I) as u8;
+            let n = (255 * z_output / max_i) as u8;
             *pixel = image::Rgb([data[0], n, data[2]]);
         }
+        print!("\t {:.2}% | {} / {}\r", 100f64*(i as f64 + 1f64) / size_y as f64, i+1, size_y);
     }
-
+    println!();
     img.save("out.png").unwrap();
-
 }
+
 
 fn main() {
     // Main function of the program
 
+    // Defines Initial Values
+    let size_x = 2048u32;
+    let size_y = 2048u32;
+    let max_i = 1024u32;
+
     // Ensures User wants to continue
     {
         // Answer check to make sure the its equals to "y"
-        println!("Are you sure you want to continue with the size {} x {} [ y | n ]?", SIZE_X, SIZE_Y);
+        println!("Are you sure you want to continue with the size {} x {} [ y | n ]?", size_x, size_y);
         let mut v: String = String::default();
         let _ = std::io::stdin().read_line(&mut v).unwrap();
 
@@ -115,12 +98,20 @@ fn main() {
             , 'y');
     }
 
-    // Configures state
-    let mut state = [ [math::Complex { real : 0f64, imaginary : 0f64 }; *SIZE_X]; *SIZE_Y ];
-    set_initial_values(&mut state);
-    let mut out_values = eval_function(&mut state);
+    let start_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
 
-    save_image(&mut out_values);
+    // Configures state
+    eval_function(size_x, size_y, max_i);
+
+    let end_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    println!("[Finished in {:.1}s]", end_time as i64 - start_time as i64);
 
     // Show Completion Message
     {
