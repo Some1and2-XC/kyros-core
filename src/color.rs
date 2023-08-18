d68f8e211ab014de09387f7b573226fd679d586f
MT <mkpt00@gmail.com>
2023-08-17 20:34:42 -0700

    Added Coloring File

---
+++ src/color.rs
@@ -0,0 +1,15 @@
+/*
+    Author : @Some1and2
+      Date : 6/21/2023
+*/
+
+use hsv::hsv_to_rgb;
+
+let backgroud_color = [255, 255, 255];
+
+pub struct colorize {
+    
+}
+
+impl colorize {i
+}
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@
 Author : @Some1and2
   Date : 6/21/2023
 
-Main file for running processes
-*/
+  Main file for running processes
+  */
 
 mod math;
 
@@ -19,116 +19,117 @@ use hsv;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn get_math_value(value: u32, max_ref: u32) -> f64 {
-    // Function for getting the mathematical space
-    // Value from "value"
-    4f64 * (value as f64) / (max_ref as f64 - 1f64) - 2f64
+	// Function for getting the mathematical space
+	// Value from "value"
+	4f64 * (value as f64) / (max_ref as f64 - 1f64) - 2f64
 }
 
 fn eval_function(size_x: u32, size_y: u32, max_i: u32) {
-    /*
-        Function for getting the value of each point
-    */
+	/*
+	   Function for getting the value of each point
+	   */
 
-    fn get_point_value( mut z: math::Complex, max_i: u32 ) -> u32 {
-        /*
-            Function for getting the value of a particular point
-            Returns the amount of iterations completed
-        */
+	fn get_point_value( mut z: math::Complex, max_i: u32 ) -> u32 {
+		/*
+		   Function for getting the value of a particular point
+		   Returns the amount of iterations completed
+		   */
 
-        // Sets Initial Value
-        let c = z.clone();
+		// Sets Initial Value
+		let c = z.clone();
 
-        // Gets Mandelbrot Value
-        for iteration in 0..max_i {
-            if z.is_greater(2f64) {
-                return iteration;
-            }
-            z = z * z + c;
-        }
-        return max_i;
-    }
+		// Gets Mandelbrot Value
+		for iteration in 0..max_i {
+			if z.is_greater(2f64) {
+				return iteration;
+			}
+			z = z * z + c;
+		}
+		return max_i;
+	}
 
-    // Sets Image Values
-    let mut img = image::ImageBuffer::new(size_x, size_y);
-    for (_x, _y, pixel) in img.enumerate_pixels_mut() {
-        *pixel = image::Rgb([255, 255, 255]);
-    }
+	// Sets Image Values
+	let mut img = image::ImageBuffer::new(size_x, size_y);
+	for (_x, _y, pixel) in img.enumerate_pixels_mut() {
+		*pixel = image::Rgb([255, 255, 255]);
+	}
 
-    // Goes through each pixel
-    for i in 0..size_y {
-        for j in 0..size_x {
+	// Goes through each pixel
+	for i in 0..size_y {
+		for j in 0..size_x {
 
-            let z = math::Complex{ real : get_math_value(j, size_x), imaginary : get_math_value(i, size_y), }; // Sets Initial Value
-            let z_output = get_point_value( z, max_i ); // Gets output of z value
-            let pixel = img.get_pixel_mut(j, i); // gets pixel reference for img[i][j]
+			let z = math::Complex{ real : get_math_value(j, size_x), imaginary : get_math_value(i, size_y), }; // Sets Initial Value
+			let z_output = get_point_value( z, max_i ); // Gets output of z value
+			let pixel = img.get_pixel_mut(j, i); // gets pixel reference for img[i][j]
 
-            // Gets color value
-            let out_rgb: (u8, u8, u8);
+			// Gets color value
+			let out_rgb: (u8, u8, u8);
 
-            if z_output == 0 {out_rgb = (255, 255, 255)}
-            else if z_output == max_i {out_rgb = (0, 0, 0)}
-            else {
-                out_rgb = hsv::hsv_to_rgb(
-                    ( 9f64 * z_output as f64 ) % 360f64,
-                    1f64,
-                    1f64,
-                    );
-                };
+			if z_output == 0 {out_rgb = (255, 255, 255)}
+			else if z_output == max_i {out_rgb = (0, 0, 0)}
+			else {
+				out_rgb = hsv::hsv_to_rgb(
+					( 9f64 * z_output as f64 ) % 360f64,
+					1f64,
+					1f64,
+					);
+			};
 
-            *pixel = image::Rgb([out_rgb.0, out_rgb.1, out_rgb.2]);
-        }
-        print!("\t {:.2}% | {} / {}\r", 100f64*(i as f64 + 1f64) / size_y as f64, i+1, size_y);
-    }
-    println!();
-    println!("Saving File!");
-    img.save("out.png").unwrap();
+			*pixel = image::Rgb([out_rgb.0, out_rgb.1, out_rgb.2]);
+		}
+		print!("\t {:.2}% | {} / {}\r", 100f64*(i as f64 + 1f64) / size_y as f64, i+1, size_y);
+	}
+	println!();
+	println!("Saving File!");
+	img.save("out.png").unwrap();
 }
 
 
 fn main() {
-    // Main function of the program
+	// Main function of the program
 
-    // Defines Initial Values
+	// Defines Initial Values
 
-    let size_x = 131_072u32;
-    let size_y = 131_072u32;
-    // let size_x = 1024u32;
-    // let size_y = 1024u32;
-    let max_i = 2048u32;
+	// let size_x = 131_072u32;
+	// let size_y = 131_072u32;
+	let size_x = 1024u32;
+	let size_y = 1024u32;
+	let max_i = 2048u32;
 
-    // Ensures User wants to continue
-    {
-        // Answer check to make sure the its equals to "y"
-        println!("Are you sure you want to continue with the size {} x {} [ y | n ]?", size_x, size_y);
-        let mut v: String = String::default();
-        let _ = std::io::stdin().read_line(&mut v).unwrap();
+	// Ensures User wants to continue
+	{
+		// Answer check to make sure the its equals to "y"
+		println!("Are you sure you want to continue with the size {} x {} [ y | n ]?", size_x, size_y);
+		let mut v: String = String::default();
+		let _ = std::io::stdin().read_line(&mut v).unwrap();
 
-        assert_eq!(v
-            .to_lowercase()
-            .chars()
-            .nth(0)
-            .unwrap()
-            , 'y');
-    }
+		assert_eq!(v
+			.to_lowercase()
+			.chars()
+			.nth(0)
+			.unwrap()
+			, 'y');
+	}
 
-    let start_time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs_f64();
+	// Sets the starting time
+	let start_time = SystemTime::now()
+		.duration_since(UNIX_EPOCH)
+		.unwrap()
+		.as_secs_f64();
 
-    // Configures state
-    eval_function(size_x, size_y, max_i);
+	// Configures state
+	eval_function(size_x, size_y, max_i);
 
-    let end_time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs_f64();
+	let end_time = SystemTime::now()
+		.duration_since(UNIX_EPOCH)
+		.unwrap()
+		.as_secs_f64();
 
-    println!("[Finished in {:.1}s]", end_time as i64 - start_time as i64);
+	println!("[Finished in {:.1}s]", end_time as i64 - start_time as i64);
 
-    // Show Completion Message
-    {
-        println!("FINISHED!");
-        let _ = std::io::stdin().read_line(&mut String::new());
-    }
+	// Show Completion Message
+	{
+		println!("FINISHED!");
+		let _ = std::io::stdin().read_line(&mut String::new());
+	}
 }
