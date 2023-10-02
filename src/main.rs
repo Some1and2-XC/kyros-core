2cb40a587fb82a1793208130fe450b7a4001cd0c
MT <04x0xx@gmail.com>
2023-10-02 19:09:46 -0300

    Added clap for CLI options

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,15 @@ mod math;
 extern crate image;
 
 use hsv;
+
 use std::env::args;
 use std::io::Write;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::collections::HashMap;
+
 use crate::math::structs;
+
+use clap::Parser;
 
 // type GenDataType = f64;
 type GenDataType = structs::Complex;
@@ -33,18 +37,30 @@ struct Config {
     gen_formula:              String, // Specifies Formula for Generator
 }
 
+/// The kyros fractal imgae generator rewritten in rust. 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The amount of pixels to generate
+    #[arg(short, long, default_value_t = 256)]
+    pixels: u32,
+
+    /// The amount of iterations to run per pixel
+    #[arg(long, default_value_t = 1024)]
+    iterations: u64,
+
+    /// The generation function to use
+    #[arg(short, long, default_value_t = ("SD".to_string()))] // The LSP lies, parentheses are needed
+    formula: String,
+}
+
+/// Function for exiting the program early with an error message. 
 fn error_exit(error_msg: String) {
-    /*
-    Function for exiting the program early with an error message. 
-     */
     print!("[Exit code : 1 | {:?}]", error_msg);
 }
 
+/// Function for easily getting input values from stdin. 
 fn input(out_msg: String) -> String {
-    /*
-    Function for easily getting input values from stdin
-    */
-
     print!("{}", out_msg);
     let _ = std::io::stdout().flush();
     let mut v: String = String::default();
@@ -52,62 +68,27 @@ fn input(out_msg: String) -> String {
     return v.trim().to_string();
 }
 
+/// Function for initializing interactive configuration. 
 fn interactive_config() -> Config {
 
     // Initializes Configuration Values
     let mut configuration = Config::default();
 
-    // Sets max_i
-    configuration.max_i = 1024;
-
-    // Gets Dimensions
-    configuration.size_x = match input("Enter image size (px): ".to_string()).parse() {
-        Ok(n) => n,
-        Err(_) => {
-            error_exit("Function generation method not found!".to_string());
-            std::process::exit(1);
-        }
-    };
-    configuration.size_y = configuration.size_x;
-
-    // Sets Generator
-    configuration.gen_formula = input("Enter generation function: ".to_string());
-
-    // Sets c & z swap
-    configuration.c_init = match input("Swap 'C' & 'Z' values? (Generate Julia Set) [ y | n ]: ".to_string()).as_str() {
-        "y" => Some(structs::Complex{real: 0.25, imaginary: 0.25}),
-        _ => None,
-    };
-
-    // Ensures User wants to continue
-    let confirmation = input(format!("Are you sure you want to continue with the size {} x {} [ y | n ]: ", configuration.size_x, configuration.size_y));
-
-    // Validates responce
-    if !(confirmation.to_lowercase().chars().nth(0).unwrap().to_string() == "y".to_string()) {
-        error_exit("Early exit, declined continue on size check".to_string());
-        std::process::exit(1);
-    }
-
-   return configuration;
+    return configuration;
 }
 
+/// Function for getting the mathematical space of a point.  
 fn get_math_value(value: u32, max_ref: u32) -> f64 {
-    // Function for getting the mathematical space
-    // Value from "value"
     4f64 * (value as f64) / (max_ref as f64 - 1f64) - 2f64
 }
 
-fn eval_function(config: Config, generator_function: &dyn Fn(structs::Complex, structs::Complex) -> GenDataType) {
-    /*
-       Function for getting the value of each point
-       */
-    // Sets Image Values
-    
+/// Function for getting image from configuration and generator function. 
+fn eval_function(config: &Config, generator_function: &dyn Fn(structs::Complex, structs::Complex) -> GenDataType) -> image::RgbImage {
+    // Unpacks Image Configuration
     let size_x: u32 = config.size_x;
     let size_y: u32 = config.size_y;
     let max_i: u64 = config.max_i;
     let c_init: Option<structs::Complex> = config.c_init;
-
     
     let mut c = math::structs::Complex { real: 0f64, imaginary: 0f64, };
     let mut z: math::structs::Complex;
@@ -126,7 +107,6 @@ fn eval_function(config: Config, generator_function: &dyn Fn(structs::Complex, s
     for (_x, _y, pixel) in img.enumerate_pixels_mut() {
         *pixel = image::Rgb([255, 255, 255]);
     }
-
 
     // Goes through each pixel
     for i in 0..size_y {
@@ -147,9 +127,7 @@ fn eval_function(config: Config, generator_function: &dyn Fn(structs::Complex, s
             loop {
                 if iteration == max_i { break; }
                 if z.is_greater(2.0) { break; }
-
                 z = generator_function(c, z);
-
                 iteration += 1;
             };
 
@@ -174,20 +152,32 @@ fn eval_function(config: Config, generator_function: &dyn Fn(structs::Complex, s
         print!("\t {:.2}% | {} / {}\r", 100f64*(i as f64 + 1f64) / size_y as f64, i+1, size_y);
     }
     println!();
-    println!("Saving File!");
-    img.save("out.png").unwrap();
+    return img;
 }
 
 
+/// Main function of the program
 fn main() {
-    // Main function of the program
-
     // Defines Initial Values
-
-    let cli_args: Vec<String> = args().collect();
+    let cli_args = Args::parse();
     println!("{:?}", cli_args);
 
-    let config: Config = interactive_config();
+    let config: Config;
+
+    if false {
+        config = interactive_config();
+    }
+
+    else {
+        config = Config {
+            count: 0,
+            c_init: None,
+            size_x: 256,
+            size_y: 256,
+            max_i: 1024,
+            gen_formula: "SD".to_string(),
+        };
+    }
 
     println!("{:?}", config);
 
@@ -214,8 +204,10 @@ fn main() {
         .unwrap()
         .as_secs_f64();
 
-    // Runs Config
-    eval_function(config, generator_function);
+    // Runs Config, gets 32 byte img object
+    let img = eval_function(&config, generator_function);
+    println!("Saving File!");
+    img.save(format!("out#{:}.png", config.count)).unwrap();
 
     // Finished Timings
     let end_time = SystemTime::now()
@@ -224,6 +216,6 @@ fn main() {
         .as_secs_f64();
 
     // Show Completion Message
-    println!("[Finished in {:.1}s]", end_time - start_time);
+    println!("[Finished in {:.2}s]", end_time - start_time);
     let _ = std::io::stdin().read_line(&mut String::new());
 }
