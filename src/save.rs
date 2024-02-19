edddd00182c1476701ffa0d4e05205578514b9d8
MT <04x0xx@gmail.com>
2024-02-19 14:23:51 -0400

    Added impl for PNG & B64

--- src/save.rs
+++ src/save.rs
@@ -2,25 +2,70 @@
 #![allow(unused_imports)]
 
 use super::*;
+use std::ops::Deref;
+use std::io::Cursor;
+
+use base64::{Engine as _, engine::general_purpose};
 
 use clap::error::ErrorKind;
 use clap::CommandFactory;
-use base64::{Engine as _, engine::general_purpose};
 
-use image::{Rgb, Pixel, ImageEncoder, ImageBuffer};
+use tempfile::tempfile;
+
+use image::{Rgb, Pixel, save_buffer, ColorType, ImageEncoder, ImageBuffer, PixelWithColorType, EncodableLayout};
 use image::flat::Error;
 use image::codecs::png::PngEncoder;
 
 pub trait Save {
-    fn method(image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>>, config: &Config) -> Result<(), Error>;
+    fn method<P, Container>(image_buffer: ImageBuffer<P, Container>, config: &Config) -> Result<(), Error>
+    where
+        P: Pixel<Subpixel = u8>,
+        P: PixelWithColorType,
+        [P::Subpixel]: EncodableLayout,
+        Container: Deref<Target = [P::Subpixel]>;
 }
 
 pub struct PNG {}
 
 impl Save for PNG {
-    fn method(image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>>, config: &Config) -> Result<(), Error> {
+    fn method<P, Container>(image_buffer: ImageBuffer<P, Container>, config: &Config) -> Result<(), Error> 
+    where
+        P: Pixel,
+        P: PixelWithColorType,
+        [P::Subpixel]: EncodableLayout,
+        Container: Deref<Target = [P::Subpixel]>,
+    {
+        // save_buffer(format!("{}.png", config.filename), image_buffer, image_buffer.width(), image_buffer.height());
         image_buffer.save(format!("{}.png", config.filename)).unwrap();
         return Ok(());
+    }
+}
+
+pub struct B64 {}
+
+impl Save for B64 {
+    fn method<P, Container>(image_buffer: ImageBuffer<P, Container>, _config: &Config) -> Result<(), Error> 
+    where
+        P: Pixel<Subpixel = u8>,
+        P: PixelWithColorType,
+        [P::Subpixel]: EncodableLayout,
+        Container: Deref<Target = [P::Subpixel]>,
+    {
+        let mut png_buf = Vec::new();
+        {
+            let encoder = PngEncoder::new(&mut png_buf);
+
+            let _ = encoder.write_image(
+                image_buffer.as_raw().as_ref(),
+                image_buffer.width(),
+                image_buffer.height(),
+                image::ColorType::Rgb8);
+        }
+
+        let mut b64 = String::new();
+        let _ = general_purpose::STANDARD.encode_string(png_buf, &mut b64);
+        print!("{}", b64);
+        Ok(())
     }
 }
 
