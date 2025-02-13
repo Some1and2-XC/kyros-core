8524442dcb1a3b92423e1459ac3539af9539f090
Some1and2 <04x0xx@gmail.com>
2025-02-13 16:47:23 -0400

    Added info to `png_test.sh`

--- png_test.sh
+++ png_test.sh
@@ -1,1 +1,4 @@
+# This script is just for simplifying testing an output image.
+# This uses [pngcheck](http://www.libpng.org/pub/png/apps/pngcheck.html) to do so.
+# This will run through the various chunks to and checksums to ensure the generated image is a proper image.
 pngcheck -cvp7t out.png
